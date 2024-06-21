5f2e40edb944b15a89576380267dafa0805acd82
Ding Jiatong <dingjiatong@gmail.com>
2024-06-21 13:11:23 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -174,10 +174,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          文思 WenSi
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          汇集语言模型，链接文字与思维的桥梁
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
